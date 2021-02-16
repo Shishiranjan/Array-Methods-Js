dddfa3cc4f3faf367a0976eb5e720a4aad3ad89b
Shishiranjan Thakur <34187897+Shishiranjan@users.noreply.github.com>
2021-02-16 21:11:26 +0545

    Add files via upload
    The pop() method removes the last element from an array and returns that element. This method changes the length of the array.

---
+++ Array_pop.js
@@ -0,0 +1,11 @@
+//The pop() method removes the last element from an array and returns that element. This method changes the length of the array.
+
+const plants = ["broccoli", "cauliflower", "cabbage", "kale", "tomato"];
+
+console.log(plants.pop());
+
+console.log(plants);
+
+plants.pop();
+
+console.log(plants);
