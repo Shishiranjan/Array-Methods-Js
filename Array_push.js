544ec0ddb49a630c0caa431000e57c3d09a86d71
Shishiranjan Thakur <34187897+Shishiranjan@users.noreply.github.com>
2021-02-16 21:07:51 +0545

    Add files via upload

---
+++ Array_push.js
@@ -0,0 +1,11 @@
+//The push() method adds one or more elements to the end of an array and returns the new length of the array.
+
+const animals = ["pigs", "goats", "sheep"];
+
+const count = animals.push("cows");
+console.log(count);
+
+console.log(animals);
+
+animals.push("chickens", "cats", "dogs");
+console.log(animals);
