513e64ed274c1c61258e8d6bcf48b3eeb512f5c9
Shishiranjan Thakur <34187897+Shishiranjan@users.noreply.github.com>
2021-02-17 11:37:51 +0545

    Add files via upload
    The splice() method changes the contents of an array by removing or replacing existing elements and/or adding new elements.

---
+++ splice_method.js
@@ -0,0 +1,12 @@
+//The splice() method changes the contents of an array by removing or replacing existing elements and/or adding new elements.
+
+const months = ["Jan", "March", "April", "June"];
+months.splice(1, 0, "Feb");
+// inserts at index 1
+console.log(months);
+// expected output: Array ["Jan", "Feb", "March", "April", "June"]
+
+months.splice(4, 1, "May");
+// replaces 1 element at index 4
+console.log(months);
+// expected output: Array ["Jan", "Feb", "March", "April", "May"]
