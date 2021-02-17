309d80cc8809bd81e9470daf03ab4a93bbbbec05
Shishiranjan Thakur <34187897+Shishiranjan@users.noreply.github.com>
2021-02-17 20:58:56 +0545

    Add files via upload
    The copy Within() method shallow copies part of an array to another location in the same array and returns it without modifying its length.

---
+++ Copywithin.js
@@ -0,0 +1,12 @@
+//The copyWithin() method shallow copies part of an array to another location in the same array
+//and returns it without modifying its length.
+
+const array1 = ["a", "b", "c", "d", "e"];
+
+// copy to index 0 the element at index 3
+console.log(array1.copyWithin(0, 3, 4));
+// expected output: Array ["d", "b", "c", "d", "e"]
+
+// copy to index 1 all elements from index 3 to the end
+console.log(array1.copyWithin(1, 3));
+// expected output: Array ["d", "d", "e", "d", "e"]
