0a1770a3af0d6b5e830087adb9f412c3269d990c
Shishiranjan Thakur <34187897+Shishiranjan@users.noreply.github.com>
2021-02-17 11:35:06 +0545

    Add files via upload
    The reverse method actually reverse the whole array in which the first element becomes the last and the last element becomes the first.

---
+++ reverse_method.js
@@ -0,0 +1,6 @@
+//The reverse method actually reverse the whole array in which the first element becomes the last and the last
+//element becomes thr first.
+
+const plants = ["broccoli", "cauliflower", "cabbage", "kale", "tomato"];
+
+console.log(plants.reverse());
