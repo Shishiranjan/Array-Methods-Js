376cc5482e8247a49ae27138b94272e3368e9e83
Shishiranjan Thakur <34187897+Shishiranjan@users.noreply.github.com>
2021-02-17 20:37:27 +0545

    Add files via upload

---
+++ includes_method.js
@@ -0,0 +1,14 @@
+//The includes() method determines whether an array includes a certain value among its entries, returning true or false as appropriate.
+
+const FirstName = [
+  "Shisiranjan",
+  "Subham",
+  "Subash",
+  "Prakash",
+  "Randhir",
+  "Suraj",
+];
+
+console.log(FirstName.includes("Shishiranjan"));
+console.log(FirstName.includes("Suraj"));
+console.log(FirstName.includes("pankaj"));
