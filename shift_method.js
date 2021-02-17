02a5ba7d8289cfe6cc42be9c5c2696e777047d15
Shishiranjan Thakur <34187897+Shishiranjan@users.noreply.github.com>
2021-02-17 11:37:06 +0545

    Add files via upload
    For removing an element from starting position we use shift method.

---
+++ shift_method.js
@@ -0,0 +1,23 @@
+//For removing an element from the starting position we use shift method.
+
+const plants = ["broccoli", "cauliflower", "cabbage", "kale", "tomato"];
+
+console.log(plants.shift());
+
+console.log(plants);
+
+plants.shift();
+
+console.log(plants);
+
+plants.shift();
+
+console.log(plants);
+
+plants.shift();
+
+console.log(plants);
+
+plants.shift();
+
+console.log(plants);
