3c048bd0c48f012da9c93d46e76ca7363e887e31
Shishiranjan Thakur <34187897+Shishiranjan@users.noreply.github.com>
2021-02-18 23:02:08 +0545

    Add files via upload
    Key method actually prints the index of every individual method of an array.

---
+++ key_method.js
@@ -0,0 +1,9 @@
+//Key method actually prints the index of every individual method of an array.
+
+const surname = ["Thakur", "shah", "jha", "yadav", "shrestha", "mandal"];
+
+const find = surname.keys();
+
+for (const key of find) {
+  console.log(key);
+}
