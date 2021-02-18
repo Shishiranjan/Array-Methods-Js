8555671e0fe02486dfba370a0701b3b699e0e9df
Shishiranjan Thakur <34187897+Shishiranjan@users.noreply.github.com>
2021-02-18 11:07:23 +0545

    Add files via upload
    This method  actually actually extract the index of an array which we are simply searching for.

---
+++ IndexOf_method.js
@@ -0,0 +1,10 @@
+//This actually actually extract the index of an array which we are simply searching for.
+//Suppose there are five elements in the original array and then we want the indexo of third element then
+//in that case we use this indexOf method.
+// it's illustration is shown below.
+
+const seat = ["4E", "5B", "6C", "7D", "8E", "9A"];
+
+const newSeat = seat.indexOf("6C");
+
+console.log(newSeat);
